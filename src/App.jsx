430507051342ccfa66f547f4d9c9b3265ca5faf1
iwmvictor <isingizwemunezerovictor5@gmail.com>
2024-02-15 15:33:21 +0200

    Integrated Vercel Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import React from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout.jsx";
 import Home from "./components/home.jsx";
 import Resume from "./components/resume.jsx";
+import { Analytics } from "@vercel/analytics/react"; // Import Analytics component
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BrowserRouter>
-       <Routes>
-         <Route path="/" element={<Layout />}>
-           <Route index element={<Home />}></Route>
-           <Route path="resume" element={<Resume />}></Route>
-         </Route>
-       </Routes>
-     </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="resume" element={<Resume />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Analytics /> {/* Include Analytics component */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
