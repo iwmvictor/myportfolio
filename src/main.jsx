4489c8787216d06f17560724d6fcc395cda8cd4e
iwmvictor <isingizwemunezerovictor5@gmail.com>
2024-02-15 08:26:35 +0200

    latest fix for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,4 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Layout from "./components/layout.jsx";
-// import Home from "./components/home.jsx";
-// import Resume from "./components/resume.jsx";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route index element={<Home />}></Route>
-//           <Route path="resume" element={<Resume />}></Route>
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
